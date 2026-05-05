b362b425fc4add21f781437db92bf320e656ab7d
abdullahtapanci <tapanciabdullah12@gmail.com>
2026-05-05 12:08:13 +0300

    removed config.py from github

--- src/config.py
+++
@@ -1,9 +0,0 @@
-IMAGE_WIDTH = 512
-IMAGE_HEIGHT = 256
-BATCH_SIZE = 8
-LEARNING_RATE = 5e-4
-DEVICE = "cuda"
-ROOT_DIR = "/content/drive/MyDrive/Lane_Detection_Project/data/tusimple" #inside tusimple -> train_set , test_set , val.txt , train.txt 
-
-
-# kaggle api key =  KGAT_9b16bfeddf69c01fa033fa0cfa69701b
